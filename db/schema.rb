108e410b988de60f66879a8e7ceb229b15c87bca
Kyle Bowman <kylebowman0414@yahoo.com>
2017-04-21 21:58:26 +0000

    Added profile and user associations

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,19 @@ ActiveRecord::Schema.define(version: 20170421010017) do
   end
 
   create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
